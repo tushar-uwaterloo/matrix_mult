37763043403c9277779bbd76c88e04b1d13fa6cc
Zhuanhao Wu <zhwu95@gmail.com>
2018-02-28 18:26:52 -0500

    tidying the directory

--- obj_dir/Vmat_vect_mult2__ALLsup.d
+++
@@ -1,5 +0,0 @@
-Vmat_vect_mult2__ALLsup.o: Vmat_vect_mult2__ALLsup.cpp \
- Vmat_vect_mult2__Syms.cpp Vmat_vect_mult2__Syms.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h Vmat_vect_mult2.h
